4d77b17f68f4f3041031538eb5a9bbb1c822f79a
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-11-24 07:50:17 +0100

    Initial Commit

---
+++ Xkcd-Comics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xkcd-Comics
+//
+//  Created by Jia-Jiuan Tsai on 2021-11-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xkcd-Comics/Xkcd_ComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xkcd_ComicsApp.swift
+//  Xkcd-Comics
+//
+//  Created by Jia-Jiuan Tsai on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct Xkcd_ComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
