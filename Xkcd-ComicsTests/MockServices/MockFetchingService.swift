a3528cff162965d0b1c55ffa777eb59a270ac088
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2022-03-04 13:15:53 +0100

    UnitTest: Set ComicsViewModel to class level

--- Xkcd-ComicsTests/MockServices/MockFetchingService.swift
+++ Xkcd-ComicsTests/MockServices/MockFetchingService.swift
@@ -8,9 +8,38 @@
 import Foundation
 @testable import Xkcd_Comics
 
+extension Comic {
+    static func makeMock(num: Int = 0) -> Self {
+        .init(num: num, title: "Test", img: "Test", alt: "Test", year: "Test", month: "Test")
+    }
+}
+
 class MockFetchingService: FetchingServiceProtocol {
+    var fetchLatestComicResult: Result<Comic, FetchingError>
+    var fetchSpecificComicResult: Result<Comic, FetchingError>
+    
+    private(set) var lastSpecificComicNumber: Int?
+    
+    init(
+        fetchLatestComicResult: Result<Comic, FetchingError> = .success(.makeMock(num: 100)),
+        fetchSpecificComicResult: Result<Comic, FetchingError> = .success(.makeMock(num: 200))
+    ) {
+        self.fetchLatestComicResult = fetchLatestComicResult
+        self.fetchSpecificComicResult = fetchSpecificComicResult
+    }
+    
     func fetchComic(comicType: ComicType, completion: @escaping (Result<Comic, FetchingError>) -> Void) {
-        let mockComic = Comic(num: 100, title: "Test", img: "Test", alt: "Test", year: "Test", month: "Test")
-        completion(.success(mockComic))
+//        let mockLatestComic = Comic(num: 100, title: "Test", img: "Test", alt: "Test", year: "Test", month: "Test")
+//        let mockSpecificComic = Comic(num: 200, title: "Test", img: "Test", alt: "Test", year: "Test", month: "Test")
+        
+        switch comicType {
+        case .latestComic:
+//            completion(.success(mockLatestComic))
+            completion(fetchLatestComicResult)
+        case .specificComic(let number):
+//            completion(.success(mockSpecificComic))
+            lastSpecificComicNumber = number
+            completion(fetchSpecificComicResult)
+        }
     }
 }
--- Xkcd-ComicsTests/Xkcd_ComicsTests.swift
+++ Xkcd-ComicsTests/Xkcd_ComicsTests.swift
@@ -13,26 +13,28 @@ import Combine
 // Testing Structure: Given, When, Then
 
 class XkcdComicsTests: XCTestCase {
+    private var viewModel: ComicsViewModel?
     private var subscriptions = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = ComicsViewModel(fetchingService: MockFetchingService(), userDataService: MockUserDataService())
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
     }
     
     func test_ComicsVM_ifSaved_isFalseInitially() {
-        let viewModel = ComicsViewModel(fetchingService: MockFetchingService(), userDataService: MockUserDataService())
-        
+        guard let viewModel = viewModel else { return }
         XCTAssertFalse(viewModel.isSaved)
     }
 
     func test_ComicsVM_isSaved_shouldBeTrue() {
         let mockComic3 = Comic(num: 2, title: "Test2", img: "Test2", alt: "Test2", year: "Test2", month: "Test2")
 
-        let viewModel = ComicsViewModel(fetchingService: MockFetchingService(), userDataService: MockUserDataService())
+        guard let viewModel = viewModel else { return }
         
         let expectation = XCTestExpectation(description: "hey")
         viewModel.$isSaved
@@ -49,10 +51,10 @@ class XkcdComicsTests: XCTestCase {
         wait(for: [expectation], timeout: 0.5)
     }
     
+    // MARK: Fetching Comics
+    
     func test_comicsViewModel_fetchComicCalledOnInit() {
-        let viewModel = ComicsViewModel(fetchingService: MockFetchingService(),
-                                        userDataService: MockUserDataService()
-        )
+        guard let viewModel = viewModel else { return }
         
         let expectation = XCTestExpectation(description: "fetchComic should be called on init")
         viewModel.$comic
@@ -63,9 +65,73 @@ class XkcdComicsTests: XCTestCase {
             }
             .store(in: &subscriptions)
         
-        wait(for: [expectation], timeout: 5.5)
+        wait(for: [expectation], timeout: 0.5)
     }
     
+    func test_comicsViewModel_fetchNextComic_shouldSuccess() {
+        guard let viewModel = viewModel else { return }
+        
+        let expectation = XCTestExpectation(description: "should be able to fetch next comic")
+        
+        viewModel.$comic
+            .dropFirst()
+            .sink { comic in
+                guard comic.num == 200 else { return }
+                
+                expectation.fulfill()
+            }
+            .store(in: &subscriptions)
+        
+        viewModel.fetchNextComic()
+        
+        wait(for: [expectation], timeout: 0.5)
+    }
+    
+    func test_comicsViewModel_fetchPreviousComic_shouldSuccess() {
+        guard let viewModel = viewModel else { return }
+        
+        let expectation = XCTestExpectation(description: "should be able to fetch previous comic")
+        viewModel.$comic
+            .dropFirst()
+            .sink { comic in
+                guard comic.num == 200 else { return }
+                expectation.fulfill()
+            }
+            .store(in: &subscriptions)
+        
+        viewModel.fetchPreviousComic()
+        
+        wait(for: [expectation], timeout: 0.5)
+    }
+    
+    func test_comicsViewModel_fetchSearchedComic_shouldSuccess() {
+        let expectedComic = Comic(num: 88, title: "", img: "", alt: "", year: "", month: "")
+        
+        let mockFetchingService = MockFetchingService(fetchSpecificComicResult: .success(expectedComic))
+        let viewModel = ComicsViewModel(
+            fetchingService: mockFetchingService,
+            userDataService: MockUserDataService()
+        )
+        
+        let expectation = XCTestExpectation(description: "should be able to fetch a searched comic")
+        viewModel.$comic
+            .dropFirst()
+            .sink { comic in
+                guard comic.num == expectedComic.num else { return }
+                
+                XCTAssertEqual(mockFetchingService.lastSpecificComicNumber, 9)
+                expectation.fulfill()
+            }
+            .store(in: &subscriptions)
+        
+        viewModel.searchComic(searchNum: "9")
+        viewModel.searchComic(searchNum: "9")
+        
+        wait(for: [expectation], timeout: 10.5)
+    }
+    
+    
+
     /*func test_ComicsVM_comicObjectList_shouldBeEmpty() {
         // Given
         // When
